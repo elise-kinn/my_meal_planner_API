3c1cb9f78f22900a7007f6feccc1af96e2a69903
Elise L <elli3183@gmail.com>
2026-04-02 11:26:25 +0200

    Add :get meals planned
    - new GET route to get meals planned by a usr between 2 dates
    - POST method updated to return the meal added th the front

--- controllers/servicesControllers.js
+++ controllers/servicesControllers.js
@@ -16,15 +16,47 @@ const addService = async (req, res) => {
 
         if(isAlreadyInserted) return res.status(400).json(isAlreadyInserted.fk_id_meal)
 
-        await db.none(`
+        const newMealId = await db.one(`
             INSERT INTO meals_users(fk_id_meal, fk_id_user, date, type_meal) 
             VALUES ($1, $2, $3, $4)
+            RETURNING id_meal_user
         `, [ id_meal, id_user, formatedDate, mealType ])
+        
+        const newMeal = await db.one(`
+            SELECT m.name_meal, TO_CHAR(mu.date, 'YYYY-MM-DD') AS date, mu.type_meal
+            FROM meals_users AS mu
+            JOIN meals AS m ON m.id_meal = mu.fk_id_meal
+            WHERE id_meal_user = $1
+        `, [ newMealId.id_meal_user ])
 
-        return res.status(201).json({ message: "OK"})
+        return res.status(201).json(newMeal)
+
     } catch (error) {
         return res.status(500).json({ message: error.message })
     }
 }
 
-export { addService }
+const getMealsofTheService = async(req, res) => {
+    const id_user = req.user.id_user
+    const start = req.query.start ? req.query.start : '2000-01-10'
+    const end = req.query.end ? req.query.end : '2999-01-01'
+
+    try {
+
+        // Ajouter JOIN avec table types pour l'icône
+        
+        const meals = await db.manyOrNone(`
+            SELECT m.name_meal, TO_CHAR(mu.date, 'YYYY-MM-DD') AS date, mu.type_meal
+            FROM meals AS m
+            JOIN meals_users AS mu ON mu.fk_id_meal = m.id_meal
+            WHERE m.fk_id_user = $1 AND mu.date >= $2 AND mu.date <= $3
+        `, [ id_user, start, end ])
+
+        return res.status(200).json(meals)
+
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
+}
+
+export { addService, getMealsofTheService }
--- routes/servicesRoutes.js
+++ routes/servicesRoutes.js
@@ -2,8 +2,9 @@ import express from 'express'
 const router = express().router
 
 import { authMidlleware } from '../middlewares/authMiddleware.js'
-import { addService } from '../controllers/servicesControllers.js'
+import { addService, getMealsofTheService } from '../controllers/servicesControllers.js'
 
 router.post('/', authMidlleware, addService)
+router.get('/',  authMidlleware, getMealsofTheService)
 
 export default router
